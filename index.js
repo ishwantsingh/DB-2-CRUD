a350f63689e23be6c9e9ff068d02e1b42978c7e5
Ishwant Singh <ishwant8@gmail.com>
2019-04-09 14:15:56 +0530

    knex, db added.Feat: get req made

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
 
 const server = express();
+
+const db = knex({
+  client: "sqlite",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  }
+});
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get("/api/zoos", async (req, res) => {
+  try {
+    const allZoos = await db("zoos");
+    res.json(allZoos);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
